5c4111e2758429055552d197afa4ced72a007b25
tuananhledev <anhtl.cv@gmail.com>
2024-12-28 14:46:39 +0700

    commit khoi chay vite + tailwindcss

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <main className='text-red-500 text-3xl font-bold'>
+      Vite App
+    </main>
+  )
+}
+
+export default App
+
+//8:33
+//https://www.youtube.com/watch?v=6_ZdDVBNiEs&t=21209s
